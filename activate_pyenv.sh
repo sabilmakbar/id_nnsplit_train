d4506aceeb2e2eced73d04a0d6680bc5f0844a01
sabilmakbar <maulana.1998@yahoo.co.id>
2023-05-19 11:52:19 +0000

    Updating bash and reqs

--- activate_pyenv.sh
+++ activate_pyenv.sh
@@ -4,9 +4,11 @@ echo "Executing 'sh' command via "$(ls -l `which sh`)""
 
 venv_name="nnsplit_train_env"
 venv_folder_path="../$venv_name"
-echo "Adding virtual env "$venv_name" to ipykernel"
 
-source $venv_name/bin/activate
+echo "Adding virtual env "$venv_name" to ipykernel"
+echo "NNSplit train environment is located on "$venv_folder_path""
+
+source $venv_folder_path/bin/activate
 # how to add pyenv to jupyter kernel:
 # https://queirozf.com/entries/jupyter-kernels-how-to-add-change-remove
 # pip install jupyter
